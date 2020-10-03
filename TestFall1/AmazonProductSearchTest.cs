d5deaf09430bcf453316f83e5447957611d9bf20
Hannes Geißler <geissler.han@gmail.com>
2020-10-03 16:59:10 +0200

    Added Setup and Navigation to Amazon.de

--- TestFall1/AmazonProductSearchTest.cs
+++ TestFall1/AmazonProductSearchTest.cs
@@ -1,21 +1,26 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TestFall1
 {
     [TestFixture]
     public class AmazonProductSearchTest
     {
-
+        string amazonUrl = "https://www.amazon.de";
+        IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl(@amazonUrl);
         }
     }
 }
