d03926087d0f2f77ce71da7ffac2b12b9e236890
Hannes Geißler <geissler.han@gmail.com>
2020-10-04 14:38:41 +0200

    added try catch for finding hero banner

--- TestFall1/AmazonProductSearchTest.cs
+++ TestFall1/AmazonProductSearchTest.cs
@@ -22,37 +22,52 @@ namespace TestFall1
         [Test]
         public void ShopAdidasShoeTest()
         {
+
+            // Go to Amazon landing page
+
             driver.Navigate().GoToUrl(@amazonUrl);
 
-            // IsCorrectPageLoadedTest
-            bool hasDesktopBanner = driver.FindElement(By.Id("desktop-banner")).Size.Height > 0;
-            bool amazonPageIsLoaded = driver.Title.StartsWith("Amazon.de:");
-
             // Assert
+            // Check if landing page is loaded correctly
+            bool expectedPageTitleStart = driver.Title.StartsWith("Amazon.de:");
+            bool hasDesktopBanner;
+            try
+            {
+                hasDesktopBanner = driver.FindElement(By.Id("desktop-banna")).Size.Height > 0;
+            }
+            catch (NoSuchElementException)
+            {
+
+                hasDesktopBanner = false;
+            }
+            Assert.IsTrue(expectedPageTitleStart, "The Title of this webpage does not start with \"Amazon.de:\"");
             Assert.IsTrue(hasDesktopBanner, "The Hero Banner from Amazon landing page has not been loaded");
-            Assert.IsTrue(amazonPageIsLoaded, "The Title of this webpage does not start with \"Amazon.de:\"");
 
             // Close Cooky Window
+
             var waitForCookyWindow = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
             var cookyWindowOkButton = waitForCookyWindow.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("sp-cc-accept")));
             cookyWindowOkButton.Click();
 
-            //IsSearchBoxTest
+            // Search for "adidas schuhe" via search box
             IWebElement searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
-
-            // Assert
-
-            //IsSearchBoxTextEqualTest
             searchBox.SendKeys("adidas schuhe");
-
             searchBox.SendKeys(Keys.Enter);
 
+            // wait for search results to load
+            var waitForSearchResults = new WebDriverWait(driver, new TimeSpan(0, 0, 5));           
+            IWebElement searchResults = waitForSearchResults.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("[data-component-type='s-search-results']")));
+
+
             // Assert
+            // Check if >= 5 shoes found.
+            // search_result_6 is the 5th Shoe found
+            bool hasFifthResult = driver.FindElement(By.CssSelector("[data-component-type='s-search-result'][data-cel-widget='search_result_6888']")).Size.Height > 0;
+            Assert.IsTrue(hasFifthResult, "No search item found at 5th position");
+
 
             // FindFirstArticleTest
-            var waitForSearchResults = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            IWebElement firstArticle = waitForSearchResults.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("[data-cel-widget='search_result_2']")));
-
+            IWebElement firstArticle = searchResults.FindElement(By.CssSelector("[data-cel-widget='search_result_2']"));
             IWebElement firstArticleLinkText = firstArticle.FindElement(By.CssSelector("h2 a"));
             firstArticleLinkText.Click();
 
