6dccb4ea9e79d684a3f8dbde0d6f730d8b20d988
Hannes Geißler <geissler.han@gmail.com>
2020-10-04 11:31:31 +0200

    Added Cooky-Window closing method

--- TestFall1/AmazonProductSearchTest.cs
+++ TestFall1/AmazonProductSearchTest.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace TestFall1
 {
@@ -14,7 +16,7 @@ namespace TestFall1
         public void Setup()
         {
             driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
+            //driver.Manage().Window.Maximize();
         }
 
         [Test]
@@ -24,17 +26,34 @@ namespace TestFall1
 
             // Assert
 
+            // Close Cooky Window
+            //var waitForCookieWindow = new WebDriverWait(driver, TimeSpan.FromSeconds(5)).Until(.ElementToBeClickable(By.XPath("//a/h3")));
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+            var cookyWindowOkButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("sp-cc-accept")));
+            cookyWindowOkButton.Click();
+
             //IsSearchBoxTest
             IWebElement searchBox = driver.FindElement(By.Id("twotabsearchtextbox"));
 
             // Assert
 
-            //IsSearchBoxTextEqual
+            //IsSearchBoxTextEqualTest
             searchBox.SendKeys("adidas schuhe");
 
             searchBox.SendKeys(Keys.Enter);
 
-            // assert
+            // Assert
+
+            // FindFirstArticleTest
+            //IWebElement searchResults = driver.FindElement(By.CssSelector("[data-component-type='s-search-results']"));
+            //IWebElement firstArticle = driver.FindElement(By.CssSelector("[data-cel-widget='search_result_2']"));
+            IWebElement firstArticle = driver.FindElement(By.CssSelector("[data-index='2']"));
+            IWebElement firstArticleLinkText = firstArticle.FindElement(By.CssSelector("h2 a"));
+            firstArticleLinkText.Click();
+
+            // SelectSize
+            SelectElement dropdownMenuSelectSize = new SelectElement(driver.FindElement(By.Id("native_dropdown_selected_size_name")));
+            dropdownMenuSelectSize.SelectByIndex(1);
         }
     }
 }
