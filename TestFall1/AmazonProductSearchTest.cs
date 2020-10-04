bbd532a35997fd8788ec56bced03eb79342de772
Hannes Geißler <geissler.han@gmail.com>
2020-10-04 15:56:43 +0200

    Added assert for article page load

--- TestFall1/AmazonProductSearchTest.cs
+++ TestFall1/AmazonProductSearchTest.cs
@@ -33,7 +33,7 @@ namespace TestFall1
             bool hasDesktopBanner;
             try
             {
-                hasDesktopBanner = driver.FindElement(By.Id("desktop-banna")).Size.Height > 0;
+                hasDesktopBanner = driver.FindElement(By.Id("desktop-banner")).Size.Height > 0;
             }
             catch (NoSuchElementException)
             {
@@ -43,7 +43,7 @@ namespace TestFall1
             Assert.IsTrue(expectedPageTitleStart, "The Title of this webpage does not start with \"Amazon.de:\"");
             Assert.IsTrue(hasDesktopBanner, "The Hero Banner from Amazon landing page has not been loaded");
 
-            // Close Cooky Window
+            // Close Popup Cookies Window
 
             var waitForCookyWindow = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
             var cookyWindowOkButton = waitForCookyWindow.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("sp-cc-accept")));
@@ -55,28 +55,58 @@ namespace TestFall1
             searchBox.SendKeys(Keys.Enter);
 
             // wait for search results to load
-            var waitForSearchResults = new WebDriverWait(driver, new TimeSpan(0, 0, 5));           
-            IWebElement searchResults = waitForSearchResults.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("[data-component-type='s-search-results']")));
+            var waitForSearchResults = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+            IWebElement firstArticle = waitForSearchResults.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("[data-cel-widget='search_result_2']")));
 
 
             // Assert
             // Check if >= 5 shoes found.
             // search_result_6 is the 5th Shoe found
-            bool hasFifthResult = driver.FindElement(By.CssSelector("[data-component-type='s-search-result'][data-cel-widget='search_result_6888']")).Size.Height > 0;
+            bool hasFifthResult;
+            try
+            {
+                hasFifthResult = driver.FindElement(By.CssSelector("[data-component-type='s-search-result'][data-cel-widget='search_result_6']")).Size.Height > 0;
+            }
+            catch (NoSuchElementException)
+            {
+
+                hasFifthResult = false;
+            }
             Assert.IsTrue(hasFifthResult, "No search item found at 5th position");
 
 
-            // FindFirstArticleTest
-            IWebElement firstArticle = searchResults.FindElement(By.CssSelector("[data-cel-widget='search_result_2']"));
+            // Find first article and click it
+            //IWebElement firstArticle = searchResults.FindElement(By.CssSelector("[data-cel-widget='search_result_2']"));
             IWebElement firstArticleLinkText = firstArticle.FindElement(By.CssSelector("h2 a"));
+
+            string articleTitleFromSearchPage = firstArticleLinkText.Text;
+
             firstArticleLinkText.Click();
 
-            // SelectSize
-            var waitForArticlePage = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
-            IWebElement dropdownMenuSelectSize = waitForArticlePage.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("native_dropdown_selected_size_name")));
+            // Check if article page is loaded
 
-            SelectElement selectSize = new SelectElement(dropdownMenuSelectSize);
-            selectSize.SelectByIndex(1);
+            var waitForArticlePage = new WebDriverWait(driver, new TimeSpan(0, 0, 5));
+            IWebElement dropdownMenuSelectSize = waitForArticlePage.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("addToCart")));
+
+            // Assert
+            string articleTitleFromArticlePage = driver.FindElement(By.Id("productTitle")).Text;
+            Assert.IsTrue(articleTitleFromArticlePage.Contains(articleTitleFromSearchPage), "The correct article page has not loaded. Article text from search does not match with title from article page");
+            //Assert.AreEqual(articleTitleFromSearchPage, articleTitleFromArticlePage, "The correct article page has not loaded. Article text from search does not match with title from article page");
+
+            // Select article size
+
+            try
+            {
+                IWebElement selectSizeElement = driver.FindElement(By.Id("native_dropdown_selected_size_name"));
+                SelectElement selectSize = new SelectElement(dropdownMenuSelectSize);
+                selectSize.SelectByIndex(1);
+            }
+            catch (NoSuchElementException)
+            {
+
+                System.Diagnostics.Debug.WriteLine("There is only one possible size");
+            }
+
 
             // AddToCart
             IWebElement addToCartButton = driver.FindElement(By.Id("add-to-cart-button"));
